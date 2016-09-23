4401ce39fb56972b72726e3cc7cf48be0abc7081
Anton <ant.filatov@gmail.com>
2016-09-23 12:39:36 +0300

    bags fixed

--- for scan matcher/main.cpp
+++ for scan matcher/main.cpp
@@ -1,4 +1,5 @@
 #include "ros/ros.h"
+#include "visualization_msgs/Marker.h"
 #include "sensor_msgs/LaserScan.h"
 #include "olson_scan_matcher.h"
 
@@ -7,8 +8,10 @@
 #include <sstream>
 #include <string>
 
+#include <math.h>
 
-void from_file_to_scan(std::ifstream& scan_file, std::ifstream& odom_file, TransformedLaserScan& tls, double& angle_min, double& angle_max, double& angle_inc) {
+
+/*void from_file_to_scan(std::ifstream& scan_file, std::ifstream& odom_file, TransformedLaserScan& tls, double& angle_min, double& angle_max, double& angle_inc) {
   std::string scan_line;
   std::string odom_line;
   getline(scan_file,scan_line);
@@ -27,23 +30,38 @@ void from_file_to_scan(std::ifstream& scan_file, std::ifstream& odom_file, Trans
   }
   odom_stream >> tls.d_x >> tls.d_y >> tls.d_yaw;
 
+}*/
+
+void from_file_to_scan(std::ifstream& scan_file, TransformedLaserScan& tls,  double& angle_min, double& angle_max, double& angle_inc) {
+  std::string scan_line;
+  getline(scan_file,scan_line);
+  std::stringstream scan_stream(scan_line);
+  double range;
+  bool is_occ;
+  scan_stream >> angle_min >> angle_max >> angle_inc >> tls.d_x >> tls.d_y >> tls.d_yaw;
+  int i = 0;
+  while (scan_stream >> range) {
+    scan_stream >> is_occ;
+    tls.points.push_back({range, angle_min+angle_inc*i, is_occ});
+    i++;
+  }
 }
 
 int main(int argv, char** argc) {
   ros::init(argv, argc, "tester");
 
   ros::NodeHandle nh;
-  ros::Publisher pub = nh.advertise<sensor_msgs::LaserScan>("/testmap",1);
+  ros::Publisher pub = nh.advertise<visualization_msgs::Marker>("/testmap",1);
   sleep(1);
 
   std::ifstream fin("/home/anton/base_scan.txt");
   std::ifstream fin_odom("/home/anton/odom.txt");
   std::string base_scan;
-  getline(fin, base_scan);
-  for (int i = 0; i < 735; i++) {
+  /*getline(fin, base_scan);
+  for (int i = 0; i < 300; i++) {
     getline(fin, base_scan);
     getline(fin_odom, base_scan);
-  }
+  }*/
   //std::cout << base_scan << std::endl;
   double ang_min,ang_max,ang_inc;
 
@@ -51,71 +69,85 @@ int main(int argv, char** argc) {
 
   Olson_scan_matcher o_s_m(1.0, 0.5, 0.1);
 
-  sensor_msgs::LaserScan msg;
+  visualization_msgs::Marker msg;
   RobotState init_pose(0.0,0.0,0.0);
   RobotState pose_delta;
   TransformedLaserScan scan;
   GridMap map;
-  from_file_to_scan(fin, fin_odom, scan, ang_min, ang_max, ang_inc);
+  from_file_to_scan(fin, scan, ang_min, ang_max, ang_inc);
 
   //first call
   o_s_m.process_scan(init_pose, scan, map, pose_delta);
   //init_pose.update(pose_delta.x, pose_delta.y, pose_delta.theta);
 
   msg.header.frame_id = "my_frame";
-  msg.angle_increment = ang_inc;
-  msg.angle_min = ang_min;
-  msg.angle_max = ang_max;
+  msg.action = visualization_msgs::Marker::ADD;
+  msg.type = visualization_msgs::Marker::POINTS;
+  msg.color.r = 1.0;
+  msg.color.g = 1.0;
+  msg.color.b = 0.0;
+  msg.color.a = 1.0;
 
+  msg.scale.x = 2;
+  msg.scale.y = 2;
+  msg.id = 0;
+
+
+  geometry_msgs::Point p;
   double min = 100000.0;
   double max = -1.0;
   for (auto point : scan.points) {
-    if(max < point.range) max = point.range;
-    if(min > point.range) min = point.range;
-    msg.ranges.push_back(point.range);
+    p.x = point.range*std::cos(point.angle);
+    p.y =  point.range*std::sin(point.angle);
+    p.z = 0;
+    msg.points.push_back(p);
   }
 
-  msg.range_min = min;
-  msg.range_max = max;
+
   pub.publish(msg);
 
   //msg.ranges.clear();
 
-  sensor_msgs::LaserScan msg2;
+  visualization_msgs::Marker msg2;
   //second call
   TransformedLaserScan scan2;
-  from_file_to_scan(fin, fin_odom, scan2, ang_min, ang_max, ang_inc);
+  from_file_to_scan(fin, scan2, ang_min, ang_max, ang_inc);
 
   init_pose.update(scan2.d_x, scan2.d_y, scan2.d_yaw);
   o_s_m.process_scan(init_pose, scan2, map, pose_delta);
-  //init_pose.update(pose_delta.x, pose_delta.y, pose_delta.theta);
+  init_pose.update(pose_delta.x, pose_delta.y, pose_delta.theta);
 
-  ros::Publisher pub2 = nh.advertise<sensor_msgs::LaserScan>("/testmapp",1);
+  ros::Publisher pub2 = nh.advertise<visualization_msgs::Marker>("/testmapp",1);
   sleep(1);
   msg2.header.frame_id = "my_frame";
-    msg2.angle_increment = ang_inc;
-    msg2.angle_min = ang_min + init_pose.theta;
-    msg2.angle_max = ang_max + init_pose.theta;
+  msg2.id = 1;
+  msg2.action = visualization_msgs::Marker::ADD;
+    msg2.type = visualization_msgs::Marker::POINTS;
+    msg2.color.r = 1.0;
+    msg2.color.g = 0.0;
+    msg2.color.b = 1.0;
+    msg2.color.a = 1.0;
+
+    msg2.scale.x = 2;
+    msg2.scale.y = 2;
 
     int i = 0;
     min = 100000.0;
     max = -1.0;
     for (auto point : scan2.points) {
-      double x = point.range * std::cos(point.angle);
-      double d_x = x - init_pose.x;
-      double y = point.range * std::sin(point.angle);
-      double d_y = y - init_pose.y;
-      msg2.ranges.push_back(std::sqrt(d_x*d_x + d_y*d_y));
-      if(max < std::sqrt(d_x*d_x + d_y*d_y)) max = std::sqrt(d_x*d_x + d_y*d_y);
-      if(min > std::sqrt(d_x*d_x + d_y*d_y)) min = std::sqrt(d_x*d_x + d_y*d_y);
-      i++;
+      p.x = point.range*std::cos(point.angle + init_pose.theta) + init_pose.x;
+      p.y = point.range*std::sin(point.angle + init_pose.theta) + init_pose.y;
+      p.z = 0;
+      msg2.points.push_back(p);
+      //if(max < point.range) max = point.range;
+      //if(min > point.range) min = point.range;
       //msg2.ranges.push_back(point.range);
     }
 
-    msg2.range_min = min;
-    msg2.range_max = max;
+
     std::cout << scan2.d_x << " " << scan2.d_y<< " " << scan2.d_yaw<< std::endl;
     std::cout << pose_delta.x << " " << pose_delta.y<< " " << pose_delta.theta<< std::endl;
+    std::cout << init_pose.x << " " << init_pose.y<< " " << init_pose.theta<< std::endl;
     pub2.publish(msg2);
 
   //init_pose.update(pose_delta.x, pose_delta.y, pose_delta.theta);
